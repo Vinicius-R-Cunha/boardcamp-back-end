84877391ce4ef77e7fb3c62561cc2108ac524836
Vinicius <vinicunha2008@gmail.com>
2022-03-05 18:31:52 -0300

    delete rent controller

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -170,8 +170,26 @@ export async function endRental(req, res) {
 
 export async function deleteRental(req, res) {
     try {
+        const { id } = req.params;
 
+        const rental = await connection.query(`
+            SELECT *
+            FROM rentals
+            WHERE id=$1`
+            , [id]);
 
+        if (rental.rowCount === 0) {
+            return res.sendStatus(404);
+        }
+
+        if (rental.rows[0].returnDate === null) {
+            return res.sendStatus(400);
+        }
+
+        await connection.query(`
+            DELETE FROM rentals
+            WHERE id=$1`
+            , [id]);
 
         res.sendStatus(200);
     } catch (error) {
